b2562fb126ead6f8d80b48f4e87920aba7bd4f4b
ri-caccavale <ri.caccavale@gmail.com>
2025-02-07 14:03:25 +0100

    urdf and navigation files alined with simulation

--- src/pkg/rover_bringup/launch/online_async_launch.py
+++ src/pkg/rover_bringup/launch/online_async_launch.py
@@ -1,10 +1,14 @@
 import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import DeclareLaunchArgument
+from launch.actions import LogInfo
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+
 
 
 def generate_launch_description():
@@ -20,6 +24,10 @@ def generate_launch_description():
         default_value=os.path.join(get_package_share_directory("rover_bringup"),
                                    'config', 'mapper_params_online_async.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+    
+    rl_launch_path = os.path.join(get_package_share_directory("rover_bringup"), 'launch', 'robot_localizer.launch.py')
+    robot_localizer_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(rl_launch_path),
+        launch_arguments={'use_sim_time': use_sim_time}.items())
 
     start_async_slam_toolbox_node = Node(
         parameters=[
@@ -32,6 +40,9 @@ def generate_launch_description():
         output='screen')
 
     ld = LaunchDescription()
+
+    # Add localization to launch description
+    ld.add_action(robot_localizer_launch)
 
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(declare_slam_params_file_cmd)
